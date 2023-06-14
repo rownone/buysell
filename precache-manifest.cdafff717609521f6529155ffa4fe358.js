5224aa109ee9dd3fd12038c1f336bfa1856ec7f0
rona <row.none@gmail.com>
2023-06-14 10:20:42 +0800

    buysell build files

---
+++ precache-manifest.cdafff717609521f6529155ffa4fe358.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f1fd11bc03403395c71a01afcfe5143",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c46b43a5b636cccc0c78",
+    "url": "/static/css/2.6cfccccb.chunk.css"
+  },
+  {
+    "revision": "9e84cabe0f80da0f9b79",
+    "url": "/static/css/main.635f70b1.chunk.css"
+  },
+  {
+    "revision": "c46b43a5b636cccc0c78",
+    "url": "/static/js/2.0a35cc23.chunk.js"
+  },
+  {
+    "revision": "413b595604c5d3d1d7baf18f18130db2",
+    "url": "/static/js/2.0a35cc23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e84cabe0f80da0f9b79",
+    "url": "/static/js/main.5b812afc.chunk.js"
+  },
+  {
+    "revision": "289b3224677aa4df42c1",
+    "url": "/static/js/runtime-main.0f9bafb5.js"
+  }
+]);
